3b6021fc8347a17257e014d0ff55f8eff80486f5
Casey Bates <cwbates8@gmail.com>
2021-11-02 12:03:27 -0700

    Create sensor calibration script and modify existing method

--- moistureSensor.py
+++ moistureSensor.py
@@ -9,23 +9,37 @@ class MoistureSensor(ReadSensor):
     # Calibrate the set moisture sensors.
     def calibrate(self):
         sCalibrateVals = []
-        print("Calibrating...")
-        for i in range(25):
+        print(f"Calibrating Sensor {self.pinNum + 1}...")
+
+        input("Hold the sensor in the air. Press 'Enter' to continue...")
+
+        for i in range(5):
             sCalibrateVals.insert(i, self.getVal())
             print("\tAdding Val: ", sCalibrateVals[i])
             time.sleep(1)
         
         self.airVal = max(sCalibrateVals)
+
+        sCalibrateVals = []
+
+        input("Submerge the sensor in water. Press 'Enter' to continue...")
+
+        for i in range(5):
+            sCalibrateVals.insert(i, self.getVal())
+            print("\tAdding Val: ", sCalibrateVals[i])
+            time.sleep(1)
+
         self.waterVal = min(sCalibrateVals)
 
-        print("Max Value: ", self.airVal)
-        print("Min Value: ", self.waterVal)
+        print("Air Value: ", self.airVal)
+        print("Water Value: ", self.waterVal)
         print("\nDone calibrating")
 
     # Maps the raw input accordingly
+    # ((input - min) * 100) / (max - min) --Casey
     def mapSensorVals(self):
         val = self.getVal()
         val = (val - self.waterVal)/(self.airVal - self.waterVal)
-        val *= 100
-        val = 100 - val
+        val *= 100 # Turn into % --Casey
+        val = 100 - val # What the heck is this for??? --Casey
         return val
---
+++ sensorCalibration.py
@@ -0,0 +1,25 @@
+import adafruit_mcp3xxx.mcp3008 as MCP
+from multiprocessing import Process
+from moistureSensor import MoistureSensor
+
+moisture_one = MoistureSensor(MCP.P0)
+moisture_two = MoistureSensor(MCP.P1)
+moisture_three = MoistureSensor(MCP.P3)
+
+# Calibrates the sensors in parallel.
+p1 = Process(target=moisture_one.calibrate())
+p1.start()
+p2 = Process(target=moisture_two.calibrate())
+p2.start()
+p3 = Process(target=moisture_three.calibrate())
+p3.start()
+
+p1.join()
+p2.join()
+p3.join()
+
+with open("calibrationValues.csv", "w") as ofile:
+	ofile.write("Sensor, AirVal, WaterVal\n")
+	sensors = [moisture_one, moisture_two, moisture_three]
+	for s in range(len(sensors)):
+		ofile.write(f"Sensor {s + 1}, {sensors[s].airVal}, {sensors[s].waterVal}\n")
